f780d384ba55aa90fd8bcbb9e1f15eed86744ad7
Mauricio Taffarel <mauruciotaffarel@gmail.com>
2020-07-06 14:58:54 -0300

    4. Parcial

---
+++ Módulo 2/Sensores/4. Sensor de temperatura/code/code.ino
@@ -0,0 +1,13 @@
+const int LM35 = A0;
+float temperatura;
+ 
+void setup() {
+  Serial.begin(115200);
+}
+ 
+void loop() {
+  temperatura = map(analogRead(LM35), 0, 1023, 0, 150);
+  Serial.print("Temperatura: ");
+  Serial.println(temperatura);
+  delay(2000);
+}
